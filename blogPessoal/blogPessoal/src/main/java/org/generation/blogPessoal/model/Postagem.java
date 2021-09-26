7994d55d591a376dca90e87b2e5a451b29c4109f
renanimpacta <santana.renan@outlook.com>
2021-09-26 18:03:18 -0300

    Atividade blog pessoal

---
+++ blogPessoal/blogPessoal/src/main/java/org/generation/blogPessoal/model/Postagem.java
@@ -0,0 +1,66 @@
+package org.generation.blogPessoal.model;
+
+import java.util.Date;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+import javax.persistence.Temporal;
+import javax.persistence.TemporalType;
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Size;
+
+@Entity  // define que a classe é uma entidade
+@Table(name = "postagem") // Cria uma tabela com nome "postagem" dentro do banco de dados
+public class Postagem {
+	
+	//atributos
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private long id;
+	
+	@NotNull
+	@Size(min = 5, max = 100)
+	private String titulo;
+	
+	@NotNull
+	@Size(min = 5, max = 500)
+	private String texto;
+	
+	@Temporal(TemporalType.TIMESTAMP)
+	private Date date = new java.sql.Date(System.currentTimeMillis());
+
+	
+	//encapsulamento
+	public long getId() {
+		return id;
+	}
+	public void setId(long id) {
+		this.id = id;
+	}
+	public String getTitulo() {
+		return titulo;
+	}
+	public void setTitulo(String titulo) {
+		this.titulo = titulo;
+	}
+	public String getTexto() {
+		return texto;
+	}
+	public void setTexto(String texto) {
+		this.texto = texto;
+	}
+	public Date getDate() {
+		return date;
+	}
+	public void setDate(Date date) {
+		this.date = date;
+	}
+	
+	
+	
+	
+	
+}
---
+++ blogPessoal/blogPessoal/src/main/java/org/generation/blogPessoal/repository/PostagemRepository.java
@@ -0,0 +1,14 @@
+package org.generation.blogPessoal.repository;
+
+import java.util.List;
+
+import org.generation.blogPessoal.model.Postagem;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+
+@Repository
+public interface PostagemRepository extends JpaRepository<Postagem, Long> {  // <tipo da entidade(tabela), Tipo do ID (ChavePrimaria))>
+	public List<Postagem> findAllByTituloContainingIgnoreCase (String titulo);
+	
+}
