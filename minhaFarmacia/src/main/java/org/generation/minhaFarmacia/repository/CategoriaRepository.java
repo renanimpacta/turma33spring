e43cde3146d8484e780a0efcab3074d10580d720
renanimpacta <santana.renan@outlook.com>
2021-10-04 01:17:57 -0300

    CRUD minhaFarmacia

---
+++ minhaFarmacia/src/main/java/org/generation/minhaFarmacia/repository/CategoriaRepository.java
@@ -0,0 +1,13 @@
+package org.generation.minhaFarmacia.repository;
+
+import java.util.List;
+
+import org.generation.minhaFarmacia.model.Categoria;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface CategoriaRepository extends JpaRepository<Categoria, Long>{ // <tipo da entidade(tabela), Tipo do ID (ChavePrimaria))>
+	
+	//Esse metodo é equivalente à: select * from tb_postagem where titulo 
+	public List<Categoria> findAllByDescricaoContainingIgnoreCase (String descricao);
+	
+}
