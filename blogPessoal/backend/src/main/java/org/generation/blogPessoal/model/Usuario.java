a414624100be00864df94aba9489bbcc21726c81
renanimpacta <santana.renan@outlook.com>
2021-10-27 22:05:17 -0300

    Implementação JUNIT

--- blogPessoal/backend/src/main/java/org/generation/blogPessoal/model/Usuario.java
+++ blogPessoal/backend/src/main/java/org/generation/blogPessoal/model/Usuario.java
@@ -1,11 +1,14 @@
 package org.generation.blogPessoal.model;
 
+import java.time.LocalDate;
+
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.Table;
 import javax.validation.constraints.Email;
+import javax.validation.constraints.NotBlank;
 import javax.validation.constraints.NotNull;
 import javax.validation.constraints.Size;
 
@@ -20,19 +23,34 @@ public class Usuario {
 	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private long id;
 	
-	@NotNull
+	@NotNull(message = "O atributo nome é obrigatório")
+	@NotBlank(message = "O atributo nome é Obrigatório!")
 	@Size(min = 5, max = 100)
 	private String nome;
 	
 	@ApiModelProperty(example = "email@email.com.br")
-	@NotNull(message = "O atributo Usuário é Obrigatório!")
+	@NotNull(message = "O atributo usuário é obrigatório")
+	@NotBlank(message = "O atributo Usuário é Obrigatório!")
 	@Email(message = "O atributo Usuário deve ser um email válido!")
 	@Size(min = 5, max = 100)
 	private String usuario;
 	
-	@NotNull
+	@NotNull(message = "O atributo nome é obrigatório")
+	@NotBlank(message = "O atributo senha é Obrigatório!")
 	@Size(min = 5, max = 100)
 	private String senha;
+	
+	// Primeiro método Construtor - Com os atributos
+	public Usuario(long id, String nome, String usuario, String senha) {
+			this.id = id;
+			this.nome = nome;
+			this.usuario = usuario;
+			this.senha = senha;
+			}
+			
+	// Segundo método Construtor - Sem os atributos
+	public Usuario() { }
+
 
 	
 	//encapsulamento
--- blogPessoal/backend/src/main/java/org/generation/blogPessoal/repository/UsuarioRepository.java
+++ blogPessoal/backend/src/main/java/org/generation/blogPessoal/repository/UsuarioRepository.java
@@ -1,5 +1,6 @@
 package org.generation.blogPessoal.repository;
 
+import java.util.List;
 import java.util.Optional;
 
 import org.generation.blogPessoal.model.Usuario;
@@ -9,6 +10,9 @@ import org.springframework.stereotype.Repository;
 @Repository
 public interface UsuarioRepository extends JpaRepository<Usuario, Long> {
 	public Optional<Usuario> findByUsuario(String usuario);
+	
+	public List<Usuario> findAllByNomeContainingIgnoreCase(String nome);
+    public Usuario findByNome(String nome);
 	
 
 }
